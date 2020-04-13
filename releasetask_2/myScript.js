b543771101ad6eb8fc3eedd4073b670acc41c6a4
yujl5555 <62171025+yujl5555@users.noreply.github.com>
2020-04-13 12:21:27 +0800

    我要发布 添加了个人中心图标yjl

---
+++ releasetask_2/myScript.js
@@ -0,0 +1,77 @@
+<<<<<<< HEAD
+//选择图片，马上预览
+
+function xmTanUploadImg(obj) {
+  var file = obj.files[0];
+  //file.size 单位为byte  可以做上传大小控制
+  console.log("file.size = " + file.size);
+
+  var reader = new FileReader();
+  //读取文件过程方法
+  reader.onloadstart = function (e) {
+    console.log("开始读取....");
+  }
+
+  reader.onprogress = function (e) {
+    console.log("正在读取中....");
+  }
+
+  reader.onabort = function (e) {
+    console.log("中断读取....");
+  }
+
+  reader.onerror = function (e) {
+    console.log("读取异常....");
+  }
+
+  reader.onload = function (e) {
+    console.log("成功读取....");
+    var img = document.getElementById("xmTanImg");
+    img.src = e.target.result;
+    //或者 img.src = this.result;  //e.target == this
+  }
+
+  reader.readAsDataURL(file)
+}
+
+/* 添加标签 */
+
+=======
+//选择图片，马上预览
+
+function xmTanUploadImg(obj) {
+  var file = obj.files[0];
+  //file.size 单位为byte  可以做上传大小控制
+  console.log("file.size = " + file.size);
+
+  var reader = new FileReader();
+  //读取文件过程方法
+  reader.onloadstart = function (e) {
+    console.log("开始读取....");
+  }
+
+  reader.onprogress = function (e) {
+    console.log("正在读取中....");
+  }
+
+  reader.onabort = function (e) {
+    console.log("中断读取....");
+  }
+
+  reader.onerror = function (e) {
+    console.log("读取异常....");
+  }
+
+  reader.onload = function (e) {
+    console.log("成功读取....");
+    var img = document.getElementById("xmTanImg");
+    img.src = e.target.result;
+    //或者 img.src = this.result;  //e.target == this
+  }
+
+  reader.readAsDataURL(file)
+}
+
+/* 添加标签 */
+
+>>>>>>> 我要发布前端 添加个人中心图标yjl
