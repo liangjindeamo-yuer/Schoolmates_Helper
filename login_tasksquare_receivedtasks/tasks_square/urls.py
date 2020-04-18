7bed6b4d24d40ada310c45c1ad45de4646f0b58b
77SWF <77SUSU@sjtu.edu.cn>
2020-04-18 16:51:56 +0800

    添加任务按类别查看和按酬劳排序功能

--- login_tasksquare_receivedtasks/tasks_square/urls.py
+++ login_tasksquare_receivedtasks/tasks_square/urls.py
@@ -5,9 +5,8 @@ app_name = 'tasks_square'
 
 urlpatterns = [
     path('', views.task_square, name='task_square'),
+    path('sort/<int:type_id>/<slug:order>',views.task_square_sort,name='task_square_sort'),
     path('check_hunt/<int:task_id>/', views.check_hunt, name='check_hunt'),
     path('hunt_task/<int:task_id>/', views.hunt_task, name='hunt_task'),
-    path('reward_up/', views.reward_up, name='reward_up'),
-    path('reward_down/', views.reward_down, name='reward_down'),
     path('<int:task_id>/task_detail', views.task_detail, name='task_detail'),
 ]
--- login_tasksquare_receivedtasks/tasks_square/views.py
+++ login_tasksquare_receivedtasks/tasks_square/views.py
@@ -2,16 +2,49 @@
 
 from django.shortcuts import render
 from hunt.models import *
-from tasks_square.models import tasks_display_order
 
 
 def task_square(request):
-    all_tasks_not_picked = Task.objects.filter(is_pickedup=False)
-    tasks_ordered_by_reward_down = all_tasks_not_picked.order_by('-task_reward')
+    username = request.session.get('username')
     task_types = TaskType.objects.all()
-    order = 'defalt'
-    return render(request, 'tasks_square/task_square.html',
-                  context={'all_tasks_not_picked': all_tasks_not_picked, 'task_types': task_types, 'order': order})
+    tasks_list = Task.objects.filter(is_pickedup=False).exclude(publisher_id=username)
+    data = {
+        'tasks_list': tasks_list,
+        'task_types': task_types,
+        'type_id': 0,
+        'username':username,
+    }
+    return render(request, 'tasks_square/task_square.html', context=data)
+
+
+def task_square_sort(request, type_id, order):
+    username = request.session.get('username')
+    task_types = TaskType.objects.all()
+
+    if order=='task_reward':
+        ordername='按酬劳升序'
+    elif order=='-task_reward':
+        ordername = '按酬劳降序'
+    else:
+        ordername='默认排序'
+
+    if type_id != 0:
+        sort = TaskType.objects.get(pk=type_id).typename
+        tasks_list = Task.objects.filter(is_pickedup=False, task_type=type_id).order_by(order).exclude(
+            publisher_id=username)
+    else:
+        sort='全部任务'
+        tasks_list = Task.objects.filter(is_pickedup=False).order_by(order).exclude(publisher_id=username)
+
+    data = {
+        'tasks_list': tasks_list,
+        'username': username,
+        'task_types': task_types,
+        'type_id': type_id,
+        'sort':sort,
+        'ordername':ordername,
+    }
+    return render(request, 'tasks_square/task_square.html', context=data)
 
 
 def check_hunt(request, task_id):
@@ -27,25 +60,6 @@ def hunt_task(request, task_id):
     task.save()
     return render(request, 'tasks_square/hunt_successfully.html', context={'task': task})
 
-
-def reward_up(request):
-    task_types = TaskType.objects.all()
-    all_tasks_not_picked = Task.objects.filter(is_pickedup=False)
-    tasks_ordered_by_reward_up = all_tasks_not_picked.order_by('task_reward')
-    order = 'reward_up'
-    return render(request, 'tasks_square/task_square.html',
-                  context={'order': order, 'tasks_ordered_by_reward_up': tasks_ordered_by_reward_up,
-                           'task_types': task_types})
-
-
-def reward_down(request):
-    task_types = TaskType.objects.all()
-    all_tasks_not_picked = Task.objects.filter(is_pickedup=False)
-    tasks_ordered_by_reward_down = all_tasks_not_picked.order_by('-task_reward')
-    order = 'reward_down'
-    return render(request, 'tasks_square/task_square.html',
-                  context={'order': order, 'tasks_ordered_by_reward_down': tasks_ordered_by_reward_down,
-                           'task_types': task_types})
 
 def task_detail(request, task_id):
     task = Task.objects.get(pk=task_id)
