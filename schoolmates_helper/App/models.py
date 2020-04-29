151faa26f2486b1069e9b543a5ef6177904477fb
liangjin <liangjindeamo@163.com>
2020-04-29 10:39:25 +0800

    手机端第五次更新

---
+++ schoolmates_helper/App/models.py
@@ -0,0 +1,85 @@
+from django.db import models
+
+
+# Create your models here.
+class MainWheel(models.Model):
+    img = models.CharField(max_length=255)
+    name = models.CharField(max_length=64)
+    trackid = models.IntegerField(default=1)
+
+    class Meta:
+        db_table = 'smh_wheel'
+
+
+class User(models.Model):
+    username = models.CharField(max_length=32, unique=True)
+    password = models.CharField(max_length=256)
+    email = models.EmailField(max_length=30, unique=True)
+    icon = models.ImageField(upload_to='icons/%Y/%m/%d/', null=True)
+    is_active = models.BooleanField(default=False)
+    is_delete = models.BooleanField(default=False)
+    rank = models.IntegerField(default=0)
+
+    tel = models.PositiveIntegerField(blank=True, null=True)
+    qq = models.CharField(max_length=20, blank=True, null=True)
+    wechat = models.CharField(max_length=20, blank=True, null=True)
+    other = models.CharField(max_length=20, blank=True, null=True)
+
+    class Meta:
+        db_table = 'smh_user'
+
+
+class TaskType(models.Model):
+    type_id = models.IntegerField(default=1)
+    typename = models.CharField(max_length=32)
+    typesort = models.IntegerField(default=1)
+
+    class Meta:
+        db_table = 'smh_tasktype'
+
+
+class Contact(models.Model):
+    type_id = models.IntegerField(default=0)
+    typename = models.CharField(max_length=32)
+    class Meta:
+        db_table = 'smh_contact_type'
+
+class Task(models.Model):
+    contact_type_publisher = models.ForeignKey(Contact, on_delete=models.CASCADE, related_name='contact_publisher',
+                                               verbose_name='发布人联系方式',db_constraint=False, null=True)
+    contact_type_hunter = models.ForeignKey(Contact, on_delete=models.CASCADE, related_name='contact_hunter',
+                                            verbose_name='委托人联系方式',db_constraint=False, null=True)
+
+    task_name = models.CharField(max_length=32)
+    task_sketch = models.CharField(max_length=512, null=True)
+    task_file = models.FileField(upload_to='task_file/%Y/%m/%d/', null=True)
+    task_type = models.ForeignKey(TaskType, default=5, on_delete=models.SET_DEFAULT)
+
+    task_time = models.DateField(blank=True, null=True)
+
+    task_reward = models.FloatField(default=0, null=True)
+
+    is_pickedup = models.BooleanField(default=False)
+    is_finished = models.BooleanField(default=False)
+    is_overtime = models.BooleanField(default=False)
+
+    publisher = models.ForeignKey(User, on_delete=models.CASCADE, related_name='publisher', verbose_name='发布人')
+    hunter = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='hunter', verbose_name='委托人',
+                               db_constraint=False, null=True)
+
+    comment_publisher = models.CharField(max_length=1000, null=True)
+    comment_hunter = models.CharField(max_length=1000, null=True)
+
+    def removehunter(self):
+        self.hunter = None
+
+    class Meta:
+        db_table = 'smh_task'
+
+
+class Revoke_reason(models.Model):
+    revoke_reason = models.CharField(max_length=512, null=True)
+    task = models.ForeignKey(Task,on_delete=models.CASCADE, related_name='revoked_tasks',db_constraint=False)
+
+    class Meta:
+        db_table = 'smh_reason'
